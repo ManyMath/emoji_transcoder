83fad05b6e2cd7c4f039971148148ee1a60aa6af
sneurlax <sneurlax@gmail.com>
2025-07-28 16:37:11 -0500

    feat: create package
    `dart create -t package emoji_transcoder`

---
+++ example/emoji_transcoder_example.dart
@@ -0,0 +1,6 @@
+import 'package:emoji_transcoder/emoji_transcoder.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/emoji_transcoder.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/emoji_transcoder_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
